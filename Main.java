9a3494a7ed5d47eaada9415e1728e12bba29af07
Hanne Roeland <hanne.roeland@itvitaelearning.nl>
2022-10-01 17:01:26 +0200

    114-115

--- Main.java
+++ Main.java
@@ -2,107 +2,98 @@ package shapes;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("what");
+        Square s = new Square();
+        s.setFillColor("Blue");
+        s.setLineColor("Red");
+        s.draw();
+        System.out.println();
+
+        Triangle t = new Triangle();
+        t.setFillColor("Purple");
+        t.setLineColor("Orange");
+        t.draw();
+        System.out.println();
+
+        Octagon o = new Octagon();
+        o.setFillColor("Green");
+        o.setLineColor("Yellow");
+        o.draw();
+        System.out.println();
     }
 }
 
-class Square {
-    private final int sides = 4;
+class Shape {
     private String fillColor;
     private String lineColor;
+
+    public Shape() {}
+
+    public String getFillColor() {
+        return fillColor;
+    }
+
+    public void setFillColor(String fillColor) {
+        this.fillColor = fillColor;
+    }
+
+    public String getLineColor() {
+        return lineColor;
+    }
+
+    public void setLineColor(String lineColor) {
+        this.lineColor = lineColor;
+    }
+
+}
+
+class Square extends Shape {
+    private final int sides = 4;
 
     public Square() {}
 
     public void draw() {
         System.out.println("I am drawing a square");
-        System.out.printf("I have %d sides", sides);
+        System.out.printf("I have %d sides\n", sides);
+        System.out.printf("My fill color is %s\n", getFillColor());
+        System.out.printf("My line color is %s\n", getLineColor());
     }
 
     public int getSides() {
         return sides;
     }
 
-    public String getFillColor() {
-        return fillColor;
-    }
+} // ends class Square
 
-    public void setFillColor(String fillColor) {
-        this.fillColor = fillColor;
-    }
-
-    public String getLineColor() {
-        return lineColor;
-    }
-
-    public void setLineColor(String lineColor) {
-        this.lineColor = lineColor;
-    }
-
-
-}
-
-class Triangle {
+class Triangle extends Shape {
     private final int sides = 3;
-    private String fillColor;
-    private String lineColor;
 
     public Triangle() {}
 
     public void draw() {
         System.out.println("I am drawing a triangle");
-        System.out.printf("I have %d sides", sides);
+        System.out.printf("I have %d sides\n", sides);
+        System.out.printf("My fill color is %s\n", getFillColor());
+        System.out.printf("My line color is %s\n", getLineColor());
     }
 
     public int getSides() {
         return sides;
     }
+} // ends class Triangle
 
-    public String getFillColor() {
-        return fillColor;
-    }
-
-    public void setFillColor(String fillColor) {
-        this.fillColor = fillColor;
-    }
-
-    public String getLineColor() {
-        return lineColor;
-    }
-
-    public void setLineColor(String lineColor) {
-        this.lineColor = lineColor;
-    }
-}
-
-class Octagon {
+class Octagon extends Shape {
     private final int sides = 8;
-    private String fillColor;
-    private String lineColor;
 
     public Octagon() {}
 
     public void draw() {
         System.out.println("I am drawing an octagon");
-        System.out.printf("I have %d sides", sides);
+        System.out.printf("I have %d sides\n", sides);
+        System.out.printf("My fill color is %s\n", getFillColor());
+        System.out.printf("My line color is %s\n", getLineColor());
     }
 
     public int getSides() {
         return sides;
     }
-
-    public String getFillColor() {
-        return fillColor;
-    }
-
-    public void setFillColor(String fillColor) {
-        this.fillColor = fillColor;
-    }
-
-    public String getLineColor() {
-        return lineColor;
-    }
-
-    public void setLineColor(String lineColor) {
-        this.lineColor = lineColor;
-    }
-}
+} // ends class Octagon
